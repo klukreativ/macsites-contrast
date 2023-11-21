04d811e1d494c8f94d84b1bcd770282056187aaf
luk44 <karl.lu@outlook.com>
2023-11-21 16:34:42 -0500

    refactored navbar and links to be dynamically called with new component

---
+++ components/link.js
@@ -0,0 +1,7 @@
+export default function Link({title, url}) {
+    return (
+        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1940 nav-item">
+            <a className="nav-link">{title}</a>
+            </li>
+    )
+}
--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,14 @@
+import Link from './link';
+
 export default function Navbar() {
+    const links = [{ title: "Home", url: "" }, { title: "About", url: "" }, { title: "Contact", url: "" }];
+
     return (
         <nav id="site-navbar" className="navbar navbar-expand-lg navbar-dark navbar-site">
             <div className="container">
                 <div id="siteMenu" className="collapse navbar-collapse">
                     <ul id="navbar-nav" className="navbar-nav mr-auto">
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1940 nav-item"><a className="nav-link">Home</a></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1940 nav-item"><a className="nav-link">About</a></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1940 nav-item"><a className="nav-link">Contact</a></li>
+                        {links.map((link) => <Link title={link.title} />)}
                     </ul>
                 </div>
             </div>
