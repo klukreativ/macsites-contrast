ca7d632127ee4e77f7af22299c6b3430d8f59a1c
luk44 <karl.lu@outlook.com>
2023-11-23 17:54:06 -0500

    implemented context

--- components/colourpicker-card.js
+++ components/colourpicker-card.js
@@ -2,6 +2,8 @@ import { BlockPicker } from 'react-color';
 import chroma from 'chroma-js';
 
 export default function ColourPickerCard({ title, colour, changeColour, textColour }) {
+    const colors=['#7a003c', '#fdbf57', '#11688d', '#a5c263', '#1b656d', '#8331a7'];
+
     return (
         <div className="col-lg-6 mb-3">
             <div className="card card-shadow border-top-0">
@@ -11,7 +13,7 @@ export default function ColourPickerCard({ title, colour, changeColour, textColo
                             width={"100%"}
                             color={colour}
                             onChangeComplete={changeColour}
-                            colors={['#7a003c']}
+                            colors={colors}
                             triangle={"hide"}
                         />
                         <div className="card-body pb-0">
@@ -19,8 +21,8 @@ export default function ColourPickerCard({ title, colour, changeColour, textColo
                             <div className="card-text mb-0">
                                 <div style={{backgroundColor: colour, color: textColour, borderRadius: "1em", padding: "0.5em 1em", width: "fit-content", margin: "1em 0"}}>Sample Text</div>
                                 <p>The colour you chose, {colour.toUpperCase()}, will be paired with a font with the value of {textColour}.</p>
-                                <p>The minimum contrast ratio recommended to meet AODA standards is 3:1.</p>
-                                <p>The contrast level between your chosen colour and the font is {Math.round(chroma.contrast(colour, textColour) * 100) / 100} which {chroma.contrast(colour, textColour) >= 3 ? 'meets' : 'does not meet'} the standard.</p>
+                                <p>The minimum contrast ratio recommended to meet AODA standards for text is 4.5:1.</p>
+                                <p>The contrast level between your chosen colour and the font is {Math.round(chroma.contrast(colour, textColour) * 100) / 100} which {chroma.contrast(colour, textColour) >= 4.5 ? 'meets' : 'does not meet'} the standard.</p>
                             </div>
                         </div>
                     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/global.css';
+import { ColourProvider } from '../context/ColourContext';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <ColourProvider>
+            <Component {...pageProps} />
+        </ColourProvider>
+    )
 }
