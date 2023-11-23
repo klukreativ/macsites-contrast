712be9e79403e576f052f1ecef6ad47c63a63138
luk44 <karl.lu@outlook.com>
2023-11-23 03:00:12 -0500

    modified content and added addl styling

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,22 @@
-export default function Footer({ title = "MacSites Contrast Checker" }) {
+export default function Footer({ title, subtitle }) {
     return (
-        <footer id="site-footer" className="grey pt-4 pb-2">
+        <footer id="site-footer" className="grey pt-4 pb-2" style={{backgroundColor: "#7a003c"}}>
             <div className="container">
                 <div className="row row-eq-height">
-                    <div className="col-xl-3 col-lg-12 text-center ">
-                        <div className="col-12">
-                            <h3>{title}</h3>
-                        </div>
+                    <div className="col-xl-3 col-lg-12 text-center text-xl-end">
+                        <h2 id="site-name" className="mr-xl-3 d-flex flex-column">
+                            <a href="/" className="tier2 order-2">{subtitle}</a>
+                            <a href="/" className="tier1 order-1">{title}</a>
+                        </h2>
                     </div>
                     <div className="col-xl-9 col-lg-12 text-center text-xl-right">
                         <div className="row row-eq-height">
-                            <div className="col-md-4 col-lg-auto flex-fill text-center text-xl-left mx-auto mb-2 mb-xl-0">
-                                <h3>Address Information</h3>
-                                <p>You can edit your footer within the ‘header and footer’ menu settings.</p>
+                            <div className="col-md-4 flex-fill text-center text-xl-start mx-auto mb-2 mb-xl-0">
                             </div>
-                            <div className="col-md-4 colg-lg-auto flex-fill text-center text-xl-left mx-auto mb-2 mb-xl-0">
-                                <h3>Contact Information</h3>
+                            <div className="col-md-4 flex-fill text-center text-xl-start mx-auto mb-2 mb-xl-0">
+                            </div>
+                            <div className="col-md-4 flex-fill text-center text-xl-start mx-auto mb-2 mb-xl-0 footer-text">
+                                <p>Media Production Services<br />Mills Library L118<br />1280 Main Street West<br />Hamilton, Ontario L8S 4L8</p><p><a href='mailto:mpsweb@mcmaster.ca'>mpsweb@mcmaster.ca</a></p>
                             </div>
                         </div>
                     </div>
