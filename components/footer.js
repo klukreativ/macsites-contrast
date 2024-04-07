9fda56621881d04d2ac432321e5f41ec52323dbb
luk44 <karl.lu@outlook.com>
2024-04-07 14:57:24 -0400

    switched A tag to Link

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import { useColourContext } from '../context/ColourContext';
+import Link from 'next/link';
 
 export default function Footer({ title, subtitle }) {
     const { primaryColour } = useColourContext();
@@ -20,7 +21,7 @@ export default function Footer({ title, subtitle }) {
                             <div className="col-md-4 flex-fill text-center text-xl-start mx-auto mb-2 mb-xl-0">
                             </div>
                             <div className="col-md-4 flex-fill text-center text-xl-start mx-auto mb-2 mb-xl-0 footer-text">
-                                <p>Media Production Services<br />Mills Library L118<br />1280 Main Street West<br />Hamilton, Ontario L8S 4L8</p><p><a href='mailto:mpsweb@mcmaster.ca'>mpsweb@mcmaster.ca</a></p>
+                                <p>Media Production Services<br />Mills Library L118<br />1280 Main Street West<br />Hamilton, Ontario L8S 4L8</p><p><Link href='mailto:mpsweb@mcmaster.ca'>mpsweb@mcmaster.ca</Link></p>
                             </div>
                         </div>
                     </div>
